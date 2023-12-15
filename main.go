281fc6a59beb7bb0b6dc79d6c17e8eb95f85600a
aswinbennyofficial <aswinbenny.official@gmail.com>
2023-12-16 00:15:47 +0530

    added a lamda function that can accept post request

--- main.go
+++ main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
+	//"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
 )
 
+type Person struct{
+	FirstName *string `json:"firstName"`
+	LastName *string `json:"lastName"`
+}
+
+type ResponseBody struct{
+	Msg *string `json:"message"`
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error){
-	log.Println("Hello world")
+	var person1 Person
 	
+	err:=json.Unmarshal([]byte(request.Body),&person1)
+
+	if err!=nil{
+		return events.APIGatewayProxyResponse{},err
+	}
+
+	var msg string
+	if person1.FirstName != nil && person1.LastName != nil {
+		msg = fmt.Sprintf("Hello %v %v", *person1.FirstName, *person1.LastName)
+	} else {
+		msg = "Hello, unnamed person"
+	}
+
+	Responsebody1:= ResponseBody{
+		Msg: &msg,
+	}
+
+	responsebytes,err:=json.Marshal(Responsebody1)
+	if err!=nil{
+		return events.APIGatewayProxyResponse{},err
+	}
+
+
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body: "This is body",
+		Body: string(responsebytes),
 	}
 
 	return response,nil
@@ -20,4 +55,5 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 func main(){
 	lambda.Start(handler)
+
 }
